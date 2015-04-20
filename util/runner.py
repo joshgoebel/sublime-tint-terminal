abb2d10ee294c1295f50cae2a3272ed32431b46c
Josh Goebel <dreamer3@gmail.com>
2015-04-20 10:35:28 -0400

    show an error in the Tint console if the subprocess fails

--- util/runner.py
+++ util/runner.py
@@ -4,6 +4,8 @@ import subprocess
 import threading
 import time
 
+SHELL_ERROR = "TINT: Could not run your shell: {}\n"
+
 
 class CommandRunner():
     def __init__(self, working_dir):
@@ -21,7 +23,7 @@ class CommandRunner():
             args = ("-l", "-c")
             prefix = ""
         else:
-            args = ("-c")
+            args = ("-c", )
             prefix = ""
             pass
 
@@ -43,18 +45,23 @@ class CommandRunner():
 
         env = os.environ
 
-        p = subprocess.Popen(command,
-                             stdin=subprocess.PIPE,
-                             stdout=subprocess.PIPE,
-                             stderr=subprocess.PIPE,
-                             cwd=self.working_dir,
-                             env=env,
-                             startupinfo=startupinfo)
-        watcher = threading.Thread(target=self.watch, args=(p,))
-        watcher.start()
+        try:
+            p = subprocess.Popen(command,
+                                 stdin=subprocess.PIPE,
+                                 stdout=subprocess.PIPE,
+                                 stderr=subprocess.PIPE,
+                                 cwd=self.working_dir,
+                                 env=env,
+                                 startupinfo=startupinfo)
+            watcher = threading.Thread(target=self.watch, args=(p,))
+            watcher.start()
 
-        stdout, stderr = p.communicate(stdin.encode(encoding="UTF-8") if stdin else None)
-        stdout, stderr = stdout.decode(), stderr.decode()
+            stdout, stderr = p.communicate(stdin.encode(encoding="UTF-8") if stdin else None)
+            stdout, stderr = stdout.decode(), stderr.decode()
+
+        except Exception as e:
+            print(e)
+            return "", SHELL_ERROR.format(e.args[1])
 
         return stdout, stderr
 
